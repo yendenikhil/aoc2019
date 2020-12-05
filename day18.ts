b00196404b90793c8b6b242e11c35567620464ec
Nikhil Yekhe <nikhil.yekhe@ing.com>
2020-12-05 19:35:02 +0100

    day 18 wip - now correctly calculating edges with weight and key requisites

--- day18.ts
+++ day18.ts
@@ -1,15 +1,16 @@
-const raw = (await Deno.readTextFile("18.in")).replaceAll(/[^\x01-\x7f]/g, "")
-  .replaceAll("\r\n", "\n");
+const raw = await Deno.readTextFile("18.in");
 const p = console.log;
 
 interface Point {
   loc: number[];
   visited: number[][];
-  keys: string[];
-  len: number;
+  key: string;
+  doors: string[];
 }
 const matchCoords = (a: number[], b: number[]) =>
   a[0] === b[0] && a[1] === b[1];
+const getAtLoc = (graph: string[][]) =>
+  (loc: number[]) => graph[loc[0]][loc[1]];
 
 const part1 = async (raw: string) => {
   const lines = raw.split("\n");
@@ -17,16 +18,19 @@ const part1 = async (raw: string) => {
   for (const line of lines) {
     graph.push(line.split(""));
   }
-  // find start
-  const start = [];
-  for (let r = 0; r < graph.length; r++) {
-    const idx = graph[r].findIndex((x) => x === "@");
-    if (idx > -1) {
-      start.push(r);
-      start.push(idx);
-      break;
+  // find key locations @, a-z
+  const findKeyLoc = (key: string): [string, number[]] => {
+    const loc = [];
+    for (let r = 0; r < graph.length; r++) {
+      const idx = graph[r].findIndex((x) => x === key);
+      if (idx > -1) {
+        loc.push(r);
+        loc.push(idx);
+        break;
+      }
     }
-  }
+    return [key, loc];
+  };
   // find possible movements
   const poss = (pt: Point) => {
     const { loc } = pt;
@@ -40,77 +44,62 @@ const part1 = async (raw: string) => {
   const filterBoundries = (p: number[]) => graph[p[0]][p[1]] !== "#";
   const filterVisited = (visited: number[][]) =>
     (p: number[]) => !visited.some((v) => matchCoords(v, p));
-  const filterMissingKey = (keys: string[]) =>
-    (p: number[]) => {
-      const pt = graph[p[0]][p[1]];
-      if (pt.search(/[A-Z]/) > -1) {
-        return keys.some((k) => k.toUpperCase() === pt);
-      }
-      return true;
-    };
+  // prepare queue with each key loaction
+  const queue: Point[] = "@abcdefghijklmnopqurstuvwxyz".split("")
+    .map((k) => findKeyLoc(k))
+    .map((loc) => {
+      return {
+        loc: loc[1],
+        visited: [],
+        key: loc[0],
+        doors: [],
+      } as Point;
+    });
 
-  // BFS
-  const mega: Point[] = [{
-    loc: start.slice(),
-    visited: [],
-    keys: ['@'],
-    len: 0,
-  }];
-  const visitedNodes: [string, string, number][] = [];
-  while (mega.length > 0) {
-    const root = mega.shift();
-    if (!root) break;
-    const paths: Point[] = [];
-    const queue: Point[] = [root];
-
-    while (queue.length > 0) {
-      const curr = queue.shift();
-      if (curr === undefined) break;
-      if (curr.keys.length === 26) {
-        return curr.visited.length;
+  // now we want to build the graph of all key to key edges and the list of doors so we can yse
+  // the door thingie later
+  const edges: [string, string, number, string[]][] = [];
+  const g = getAtLoc(graph);
+  while (queue.length > 0) {
+    const curr = queue.shift();
+    // p({curr})
+    if (!curr) break;
+    // is curr a key, add on edge and terminate
+    if (g(curr.loc).search(/[a-z@]/) > -1 && curr.key !== g(curr.loc)) {
+      if (!edges.some((e) => e[0] === curr.key && e[1] === g(curr.loc))) {
+        // the edge is not found for the key - key combination then only add it.
+        // Due to nature of BFS we will always find the shortest path for edge first
+        // hence no need to check whether the current edge is smaller or not.
+        edges.push([curr.key, g(curr.loc), curr.visited.length, curr.doors]);
       }
-      const visited = curr.visited.slice();
-      const keys = curr.keys.slice();
-      visited.push(curr.loc);
-      const str = graph[curr.loc[0]][curr.loc[1]];
-      if (str.search(/[a-z]/) > -1 && !curr.keys.some((k) => k === str)) {
-        if (
-          !paths.some((p) => matchCoords(p.loc, curr.loc)) &&
-          !visitedNodes.some((n) => n[0] === curr.keys[0] && n[1] === str)
-        ) {
-          // p({ str, l: curr.visited.length, keys });
-          visitedNodes.push([curr.keys[0], str, visited.length]);
-          paths.push({
-            loc: curr.loc.slice(),
-            keys: [str],
-            visited: [],
-            len: visited.length,
-          });
-        }
-        continue;
-      }
-      const fv = filterVisited(curr.visited);
-      const fk = filterMissingKey(curr.keys);
-      const next: Point[] = poss(curr)
-        .filter(filterBoundries)
-        .filter(fv)
-        .filter(
-          fk,
-        ).map((p) => {
-          return {
-            visited,
-            keys,
-            loc: p,
-            len: 0,
-          };
-        });
-      queue.push(...next);
+      // we need not go further as key is found
+      continue;
     }
-    // p({ paths });
-    mega.push(...paths);
+
+    // is curr a door, add and proceed
+    const doors = curr.doors.slice();
+    if (g(curr.loc).search(/[A-Z]/) > -1) {
+      doors.push(g(curr.loc));
+    }
+    // add curr to visited
+    const visited = curr.visited.slice();
+    visited.push(curr.loc);
+
+    // check all next possibilities which are not walls and not visited
+    const next = poss(curr)
+      .filter(filterBoundries)
+      .filter(filterVisited(visited))
+      .map((loc) => {
+        return {
+          loc,
+          key: curr.key,
+          visited,
+          doors,
+        } as Point;
+      });
+    queue.push(...next);
   }
-  p({ visitedNodes });
-  p(visitedNodes.length);
+  p(edges);
   return -1;
 };
 
